3149b84805feee92f7ab5bd71ceeab6dd6f4cc53
emmanuel-01OA <assoko0005@gmail.com>
2023-05-06 15:07:36 +0000

    ajout widget

--- app/src/main/java/com/example/networkot/MainActivity.kt
+++ app/src/main/java/com/example/networkot/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.networkot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.bouton_up_menu, menu)
+        return true
+    }
+
+   /* override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.new_game -> {
+                newGame()
+                true
+            }
+            R.id.help -> {
+                showHelp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    } */
+
 }
