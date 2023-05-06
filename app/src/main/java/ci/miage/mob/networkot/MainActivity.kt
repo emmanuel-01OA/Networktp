4b957390c33fb346b61ebc3264f20bfca6495043
emmanuel-01OA <assoko0005@gmail.com>
2023-05-06 22:13:19 +0000

    menu nav

--- app/src/main/java/ci/miage/mob/networkot/MainActivity.kt
+++ app/src/main/java/ci/miage/mob/networkot/MainActivity.kt
@@ -2,11 +2,20 @@ package ci.miage.mob.networkot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.bouton_up_menu, menu)
+        return true
     }
 }
