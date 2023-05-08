5541ade28a3f8cf144d00104e2899451dc7d6482
emmanuel-01OA <assoko0005@gmail.com>
2023-05-08 17:57:37 +0000

    bouton reinialiser ok

--- app/src/main/java/com/example/networkot/Connexionlgn.kt
+++ app/src/main/java/com/example/networkot/Connexionlgn.kt
@@ -1,9 +1,9 @@
 package com.example.networkot
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class Connexionlgn @JvmOverloads constructor(
@@ -11,11 +11,87 @@ class Connexionlgn @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     // regarde Tanoh ici j utlise la methode drawline pour dessiner les connexions
 //
-    override fun onDraw(canvas: Canvas?) {
+    /*override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         val dessin = Paint()
-        canvas?.drawLine(0f ,0f, 300f , 300f ,dessin)
+        canvas?.drawCircle(0f ,0f, 1300f , dessin)
+
+
+          val connexionPath = Path()
+          connexionPath.moveTo(700f,1400f)
+          connexionPath.lineTo(1000f,1400f)
+          connexionPath.close()
+          dessin.stokewidht = 3f
+          dessin.setColor(Color.GREEN)
+          dessin.style = Paint.Style.STROKE
+          canvas?.drawPath(connexionPath,dessin)
+    }*/
+
+    private var PointDepart : PointF? = null
+    private var PointArrive : PointF? = null
+    private var dessin : Paint? = null
+    private var Etatdessin = false
+
+    init{
+        dessin = Paint()
+        dessin!!.color = Color.BLACK
+        dessin!!.strokeWidth = 10f
+        dessin!!.isAntiAlias = true
+
     }
+
+    override fun onDraw(dessn :Canvas? ){
+        super.onDraw(dessn)
+
+        if(Etatdessin){
+            dessn!!.drawLine(PointDepart!!.x, PointDepart!!.y , PointArrive!!.x, PointArrive!!.y ,
+                dessin!!
+            )
+        }else{
+
+        }
+    }
+
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        when (event!!.action) {
+            MotionEvent.ACTION_DOWN -> {
+                PointDepart = PointF(event.x, event.y)
+                PointArrive = PointF()
+                Etatdessin = true
+
+
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                //PointDepart = PointF(event.x,)
+                PointArrive!!.x = event.x
+                PointArrive!!.y = event.y
+                invalidate()
+
+            }
+            MotionEvent.ACTION_UP -> {
+                PointDepart = PointF(event.x, event.y)
+                PointArrive = PointF()
+                Etatdessin = false
+                invalidate()
+
+
+            }
+            else -> {
+
+            }
+        }
+
+        return true
+
+
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/networkot/MainActivity.kt
+++ app/src/main/java/com/example/networkot/MainActivity.kt
@@ -1,62 +1,105 @@
 package com.example.networkot
 
-import android.graphics.drawable.Drawable
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
-=======
 import android.view.Menu
 import android.view.MenuInflater
-import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.res.ResourcesCompat
+import android.view.View
+import android.widget.Toast
+import com.example.networkot.testdessin.Companion.CouleurB
+import com.example.networkot.testdessin.Companion.ListCouleur
+import com.example.networkot.testdessin.Companion.pathlist
+
 
 class MainActivity : AppCompatActivity() {
 
->>>>>>> a27b74244e37deab50eb3aed3051a31e96ed908b
+    companion object
+    {
+        var path = Path()
+        var dessinP = Paint()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        setupWithNavController(bottomNavigationView , navController)
-=======
-         }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.bouton_up_menu, menu)
-        return true
->>>>>>> a27b74244e37deab50eb3aed3051a31e96ed908b
+
+        // reccuperation id du bouton Ajout d objet
+       val btnAJoutObjet = findViewById<View>(R.id.add)
+
+        // reccuperation id du bouton reunitialiser
+        val btnReinitialise = findViewById<View>(R.id.btnreset)
+
+
+
+/*
+
+        // reccuperation id du bouton francais
+        val btnfrn = findViewById<View>(R.id.btnfr)
+*/
+
+        // une action listener pour le boutou ajout objet lorsqu on appuis
+        btnAJoutObjet.setOnClickListener{
+
+            Toast.makeText(this,"creation de l objet",Toast.LENGTH_SHORT).show()
+
+        }
+
+        btnReinitialise.setOnClickListener{
+
+            Toast.makeText(this,"reinitialisation  effectuee",Toast.LENGTH_SHORT).show()
+
+            pathlist.clear()
+            ListCouleur.clear()
+            path.reset()
+        }
+
+   /*     btnAng.setOnClickListener{
+
+            Toast.makeText(this,"creation de l objet",Toast.LENGTH_SHORT).show()
+
+        }
+        btnfrn.setOnClickListener{
+
+            Toast.makeText(this,"creation de l objet",Toast.LENGTH_SHORT).show()
+
+        }
+*/
     }
 
 
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.bouton_up_menu, menu)
 
-   /* override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
-        return when (item.itemId) {
-            R.id.new_game -> {
-                newGame()
-                true
-            }
-            R.id.help -> {
-                showHelp()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    } */
+
+        return super.onCreateOptionsMenu(menu)
+
+        // reccuperation id du bouton Anglais
+        val btnAng = menu?.findItem(R.id.btnang)
+    }
+
+    fun parametrClr(color: Int){
+        CouleurB = color
+        path = Path()
+    }
+
+     fun onDraw(canvas: Canvas?) {
+
+        val dessin: Paint = Paint()
+        dessin.setColor(Color.MAGENTA)
+        dessin.strokeWidth = 10f
+        canvas?.drawCircle(0f,0f,700f,dessin)
+
+
+    }
+    // couleur change
+
 
 }
